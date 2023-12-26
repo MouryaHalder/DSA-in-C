d4c830982a2a9df99ad95263244b4582448f72a6
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2023-12-26 13:29:51 +0530

    Add files via upload
    Basics

---
+++ P2_1.c
@@ -0,0 +1,15 @@
+/*P2.1 Program to input values into an array and display them*/
+#include<stdio.h>
+int main()
+{
+	int arr[8],i;
+	for(i=0; i<8; i++)
+	{
+		printf("Enter the value for arr[%d] : ",i);
+		scanf("%d", &arr[i]);
+	}
+	printf("The array elements are : \n");
+	for(i=0; i<8; i++)
+		printf("%d\t", arr[i]);
+	printf("\n");
+}
